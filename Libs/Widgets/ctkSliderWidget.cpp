384492e1410aeb66f4ab8525d46c925f19f285a2
Miklos Espak <m.espak@ucl.ac.uk>
2016-08-04 15:54:54 +0100

    Bug #60: Assert failure in ctkSliderWidget temporary fix
    This may not be the right fix, however it solves the problem here.

--- Libs/Widgets/ctkSliderWidget.cpp
+++ Libs/Widgets/ctkSliderWidget.cpp
@@ -303,7 +303,7 @@ void ctkSliderWidget::setValue(double _value)
   d->Changing = false;
   d->SpinBox->setValue(_value);
   // Why do we need to set the value to the slider ?
-  //d->Slider->setValue(d->SpinBox->value());
+  d->Slider->setValue(d->SpinBox->value());
   //double spinBoxValue = d->SpinBox->value();
   Q_ASSERT(d->equal(d->SpinBox->minimum(),d->Slider->minimum()));
   Q_ASSERT(d->equal(d->SpinBox->value(),d->Slider->value()));
